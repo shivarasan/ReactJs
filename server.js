bb01f6dd909276d50a9ba6885eed752ec120e219
sivarasan ponnuthurai <rasanthurai1992@gmail.com>
2018-03-21 17:14:08 +0530

    third commit

--- server.js
+++ server.js
@@ -13,24 +13,31 @@
 // app.listen(3001, function() {
 //     console.log('app is listening with 3001');
 // })
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
+// const app = express();
+
+// // Server routes...
+// app.get('*', (req, res) => res.send({ hi: 'there' }));
+
+// if (process.env.NODE_ENV !== 'production') {
+//   const webpackMiddleware = require('webpack-dev-middleware');
+//   const webpack = require('webpack');
+//   const webpackConfig = require('./webpack.config.js');
+//   app.use(webpackMiddleware(webpack(webpackConfig)));
+// } else {
+//   app.use(express.static('dist'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/index.html'));
+//   });
+// }
+
+// app.listen(process.env.PORT || 3050, () => console.log('Listening'));
+
+const express = require('express');
+const port = process.env.PORT || 8080;
 const app = express();
 
-// Server routes...
-app.get('*', (req, res) => res.send({ hi: 'there' }));
-
-if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
-  app.use(express.static('dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
-
-app.listen(process.env.PORT || 3050, () => console.log('Listening'));
+app.use(express.static(__dirname + '/'));
+app.listen(port);
